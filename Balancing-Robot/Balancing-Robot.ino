73970ffc52209fe4e2f54147cd2506247bc7ca30
amir tadros <amirtadros01@gmail.com>
2023-06-29 18:13:15 -0700

    uncoupled speed control using class

--- Balancing-Robot/Balancing-Robot.ino
+++ Balancing-Robot/Balancing-Robot.ino
@@ -1,50 +1,131 @@
 // Define pin connections & motor's steps per revolution
-#define dirPin1 A1
-#define stepPin1 A0
-#define dirPin2 A2
-#define stepPin2 A3
-const int stepsPerRevolution = 200;
+#include <Arduino_LSM6DS3.h>
+#include <WiFiNINA.h>
 
-void setup()
-{
-	// Declare pins as Outputs
-	pinMode(stepPin1, OUTPUT);
-	pinMode(dirPin1, OUTPUT);
-	pinMode(stepPin2, OUTPUT);
-	pinMode(dirPin2, OUTPUT);
+#define DIR_PIN_1 A1
+#define STEP_PIN_1 A0
+#define DIR_PIN_2 A2
+#define STEP_PIN_2 A3
+
+class Motor{
+public:     
+  void stop(void) { enable = false; }
+  
+  void start(void) { enable = true; }
+  
+  void init(int _stepPin, int _dirPin, uint64_t _delayTime, bool _direction) {
+    stepPin     = _stepPin;
+    dirPin      = _dirPin;
+    delayTime   = _delayTime;
+    direction   = _direction;
+
+    togglePulse = LOW;
+    enable      = false;
+
+    pinMode(stepPin, OUTPUT);
+    pinMode(dirPin, OUTPUT);
+  }
+  
+  void update(void) {
+    currentTime = micros();
+    digitalWrite(dirPin, direction);
+    if (enable == 1) {
+      if ((currentTime - deltaTime) > delayTime) {
+        pulseCount++;
+
+        if (pulseCount % 2 == 0) {
+          stepCount++;
+        }
+
+        togglePulse = togglePulse == LOW ? HIGH : LOW;
+        digitalWrite(stepPin, togglePulse);
+        deltaTime = currentTime;
+      }
+    }
+  }
+
+  void changeSpeed(uint64_t _speed) { delayTime = _speed; }
+
+  void changeDirection(bool _direction) { direction = _direction; }
+
+  uint64_t steps(void) { return stepCount; }
+
+private:
+  uint64_t delayTime, deltaTime, currentTime;
+  uint64_t pulseCount = 0;
+  uint64_t stepCount = 0;
+  int32_t stepPin, dirPin;
+  bool direction, togglePulse, enable;
+};
+
+Motor stepperOne, stepperTwo;
+uint64_t blinkTime;
+
+void InitLED();
+void InitMotors();
+void InitGyro();
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Serial Initialized!");
+
+  InitLED();
+  InitMotors();
+  InitGyro();
 }
 
-void loop()
-{
-	// Set motor direction clockwise
-	digitalWrite(dirPin1, HIGH);
-	digitalWrite(dirPin2, HIGH);
+void loop() {
+  stepperOne.update();
+  stepperTwo.update();
+  
+  if (stepperOne.steps() == 2000){
+    stepperOne.changeDirection(LOW);
+    stepperOne.changeSpeed(600);
+  }
 
-	// Spin motor slowly
-	for(int x = 0; x < stepsPerRevolution; x++)
-	{
-    digitalWrite(stepPin1, HIGH);
-    digitalWrite(stepPin2, HIGH);
-		delayMicroseconds(2000);
-    digitalWrite(stepPin1, LOW);
-    digitalWrite(stepPin2, LOW);
-		delayMicroseconds(2000);
-	}
-	delay(1000); // Wait a second
-	
-	// Set motor direction counterclockwise
-	digitalWrite(dirPin1, LOW);
-	digitalWrite(dirPin2, LOW);
+  float x, y, z;
 
-	// Spin motor quickly
-	for(int x = 0; x < stepsPerRevolution; x++)
-	{
-    digitalWrite(stepPin1, HIGH);
-    digitalWrite(stepPin2, HIGH);
-		delayMicroseconds(1000);
-    digitalWrite(stepPin1, LOW);
-    digitalWrite(stepPin2, LOW);
-		delayMicroseconds(1000);
-	}
-	delay(1000); // Wait a second
+  if (IMU.gyroscopeAvailable()) {
+    IMU.readGyroscope(x, y, z);
+
+    Serial.print(x);
+    Serial.print('\t');
+    Serial.print(y);
+    Serial.print('\t');
+    Serial.println(z);
+  }
+
+  if (millis() >= blinkTime) {
+    digitalWrite(LEDR, digitalRead(LEDR) ? LOW : HIGH);
+    blinkTime += 500;
+  }
+}
+
+void InitLED() {
+  pinMode(LEDR, OUTPUT);
+  digitalWrite(LEDR, HIGH);
+
+  blinkTime = millis() + 500;
+}
+
+void InitMotors() {
+  stepperOne.init(STEP_PIN_1, DIR_PIN_1, 1000, HIGH);
+  stepperTwo.init(STEP_PIN_2, DIR_PIN_2, 1000, HIGH);
+  stepperOne.start();
+  stepperTwo.start();
+
+  Serial.println("Motors Initialized!");
+}
+
+void InitGyro() {
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+  }
+
+  Serial.print("Gyroscope sample rate = ");
+  Serial.print(IMU.gyroscopeSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.println("Gyroscope in degrees/second");
+  Serial.println("X\tY\tZ");
 }
